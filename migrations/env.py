58bcf522b4b84b8ba8baa1ddbb29e593a4970256
Oleksandr Styhar <ostyhar@gmail.com>
2026-05-29 12:19:34 +0200

    Prepare Alembic

--- migrations/env.py
+++ migrations/env.py
@@ -3,6 +3,10 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+import app.models  # noqa: F401
+from app.db.base import Base
+
+target_metadata = Base.metadata
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -12,11 +16,6 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -72,7 +71,3 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
-from app.db.base import Base
-
-target_metadata = Base.metadata
